038a65a383254fea286a4192ef1191f154a7711d
Yassine Ben Abdelaziz <benabdelazizyassine1@gmail.com>
2024-09-28 22:53:16 +0100

    Add jwt strategy

--- backend/src/api/v1/auth/auth.controller.ts
+++ backend/src/api/v1/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from 'src/guards/local-auth/local-auth.guard';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor() {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
