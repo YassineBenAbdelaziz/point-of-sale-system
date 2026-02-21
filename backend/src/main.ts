de5b7e0e596ffca7f54f5d2c827e3a2137a766ea
YassineBenAbdelaziz <benabdelazizyassine1@gmail.com>
2026-02-21 21:07:06 +0100

    Add Swagger docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Point of Sale Documentation')
+    .setDescription('Description of POS api routes')
+    .setVersion('1')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   app.setGlobalPrefix('api');
   app.enableVersioning({
